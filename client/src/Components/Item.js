a3946df146b4bcfd2da4c28e74ce6dc0428e7a49
joshuamathia <joshuamathia@pnch.xyz>
2021-12-16 17:19:31 -0800

    Project Initalization backend and frontend. Removed sample files, created Item Api

---
+++ client/src/Components/Item.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Container } from "@mui/material/Container";
+
+const Item = () => {
+
+    return (
+
+        <>
+            <h3>Hello From Item.js</h3>
+        </>
+    )
+}
+export default Item;
