5f60e09c5b47d7a602ac0505c40e6be933870882
joshuamathia <joshuamathia@pnch.xyz>
2022-01-29 13:10:49 -0800

    Adding new components. Working on the app bar and component routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import ItemList from "./Components/ItemList";
-import Home from "./Components/Home";
-import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+
+import Footer from "./Components/Footer";
+import { Link } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="items" element={<ItemList />} />
-      </Routes>
+    <div className="app-container">
+      <h1>Welcome to the app!</h1>
+      <nav>
+        <Link to="/itemList">Items</Link>
+        <Link to="/login">Login</Link>
+      </nav>
+      <Footer />
     </div>
   );
 }
-
 export default App;
---
+++ client/src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <ul>
+        <li>yes</li>
+        <li>hello</li>
+      </ul>
+    </>
+  );
+};
+export default Navbar;
