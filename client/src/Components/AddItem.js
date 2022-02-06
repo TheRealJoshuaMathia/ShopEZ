c93a6786245dfea96289335988382273d520b36d
joshuamathia <joshuamathia@pnch.xyz>
2022-02-05 19:56:21 -0800

    Added buttons. Need to fix spacing

--- client/src/Components/AddItem.js
+++ client/src/Components/AddItem.js
@@ -1,180 +1,219 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import ItemDataService from "../services/items.service";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Grid from "@mui/material/Grid";
+import MenuItem from "@mui/material/MenuItem";
 
-// AddItem Styling
+//AddItem Styling
 
-// const styles = createTheme({
-
-//     components: {
-//     Box: {
-//         styleOverrides: {
-
-//         }
-//     }
-// }
-
-
-// });
-
-
+const styles = createTheme({
+  components: {
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          ".form-item": {
+            width: "50%",
+          },
+        },
+      },
+    },
+  },
+});
+const stores = [
+  {
+    value: "TBD",
+    label: "TBD",
+  },
+  {
+    value: "Costco",
+    label: "Costco",
+  },
+  {
+    value: "Cash & Carry",
+    label: "Cash & Carry",
+  },
+  {
+    value: "Winco",
+    label: "Winco",
+  },
+  {
+    value: "Walmart",
+    label: "Walmart",
+  },
+];
 export default class AddItem extends Component {
-    constructor(props) {
-        super(props);
-        this.onChangeTitle = this.onChangeTitle.bind(this);
-        this.onChangeType = this.onChangeType.bind(this);
-        this.onChangeCatagory = this.onChangeCatagory.bind(this);
-        this.onChangeStore = this.onChangeStore.bind(this);
-        this.saveItem = this.saveItem.bind(this);
-        this.newItem = this.newItem.bind(this);
+  constructor(props) {
+    super(props);
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangeType = this.onChangeType.bind(this);
+    this.onChangeCatagory = this.onChangeCatagory.bind(this);
+    this.onChangeStore = this.onChangeStore.bind(this);
+    this.saveItem = this.saveItem.bind(this);
+    this.newItem = this.newItem.bind(this);
 
-        this.state = {
-            id: null,
-            title: "",
-            type: "",
-            catagory: "",
-            store: "",
-            submitted: false
-        };
-    }
+    this.state = {
+      id: null,
+      title: "",
+      type: "",
+      catagory: "",
+      store: "",
+      submitted: false,
+    };
+  }
 
-    onChangeTitle(e) {
+  onChangeTitle(event) {
+    this.setState({
+      title: event.target.value,
+    });
+  }
+
+  onChangeType(event) {
+    this.setState({
+      type: event.target.value,
+    });
+  }
+  onChangeCatagory(event) {
+    this.setState({
+      catagory: event.target.value,
+    });
+  }
+
+  onChangeStore(event) {
+    this.setState({
+      store: event.target.value,
+    });
+  }
+  saveItem() {
+    var data = {
+      title: this.state.title,
+      type: this.state.type,
+      catagory: this.state.catagory,
+      store: this.state.store,
+    };
+
+    ItemDataService.create(data)
+      .then((response) => {
         this.setState({
-            title: e.target.value
+          id: response.data.id,
+          title: response.data.title,
+          type: response.data.type,
+          catagory: response.data.catagory,
+          store: response.data.store,
+
+          submitted: true,
         });
-    }
+        console.log(data);
+      })
+      .catch((event) => {
+        console.log(event);
+      });
+  }
+  newItem() {
+    this.setState({
+      title: "",
+      type: "",
+      catagory: "",
+      store: "",
+      submitted: false,
+    });
+  }
 
-    onChangeType(e) {
-        this.setState({
-            type: e.target.value
-        });
-    }
-    onChangeCatagory(e) {
-        this.setState({
-            catagory: e.target.value
-        });
-    }
-
-    onChangeStore(e) {
-        this.setState({
-            store: e.target.value
-        });
-    }
-
-    const[store, setStore] = React.useState()
-    const stores = [
-        {
-            value: 'USD',
-            label: '$',
-        },
-        {
-            value: 'EUR',
-            label: '€',
-        },
-        {
-            value: 'BTC',
-            label: '฿',
-        },
-        {
-            value: 'JPY',
-            label: '¥',
-        },
-    ];
-
-    saveItem() {
-        var data = {
-            title: this.state.title,
-            type: this.state.type,
-            catagory: this.state.catagory,
-            store: this.state.store
-        };
-
-        ItemDataService.create(data)
-            .then(response => {
-                this.setState({
-                    id: response.data.id,
-                    title: response.data.title,
-                    type: response.data.type,
-                    catagory: response.data.catagory,
-                    store: response.data.store,
-
-                    submitted: true
-                });
-                console.log(data);
-            })
-            .catch(e => {
-                console.log(e);
-            });
-    }
-    newItem() {
-        this.setState({
-            title: "",
-            type: "",
-            catagory: "",
-            store: "",
-            submitted: false
-        });
-    }
-
-    render() {
-        return (
-            <form>
-                {this.state.submitted ? (
-                    <div>
-                        <h4>Submitted successfully!</h4>
-                        <Button variant="contained" color="success">Success</Button>
-                    </div>
-                ) : (
-                    <Grid container
-                        marginTop={1}
-                        justifyContent="center"
-                        alignItems="center"
-                        sx={{
-                            background: "red",
-                            minHeight: "100vh"
-                        }}
-                    >
-                        <Grid
-                            container
-                            display="flex"
-                            direction="column"
-                            paddingTop={0}
-                            spacing={0}
-                            justifyContent="center"
-                            wrap="nowrap"
-                            alignItems="center"
-                            sx={{ background: "gray", height: "60vh", width: "50vh" }}>
-                            <Grid item marginTop={5} marginBottom={5} >
-                                <TextField required id="outlined-basic" label="Title" variant="outlined" />
-                            </Grid>
-                            <Grid item marginBottom={5}>
-                                <TextField id="outlined-basic" label="Type" variant="outlined" />
-                            </Grid>
-                            <Grid item marginBottom={5}>
-                                <TextField
-                                    id="outlined-basic"
-                                    label="Catagory"
-                                    variant="outlined"
-                                    select
-                                    value={store}>
-                                    {stores.map((option) => (
-                                        <MenuItem key={option.value} value={option.value}>
-                                            {option.label}
-                                        </MenuItem>
-                                    ))}
-                                </TextField>
-                            </Grid>
-                            <Grid item marginBottom={5}>
-                                <TextField id="outlined-basic" label="Store" variant="outlined" />
-                            </Grid>
-                        </Grid>
+  render() {
+    return (
+      <ThemeProvider theme={styles}>
+        <form>
+          {this.state.submitted ? (
+            <div>
+              <h4>Submitted successfully!</h4>
+              <Button variant="contained" color="success">
+                Success
+              </Button>
+            </div>
+          ) : (
+            <Grid
+              container
+              marginTop={3}
+              justifyContent="center"
+              alignItems="center"
+              sx={{
+                background: "white",
+                minHeight: "50vh",
+              }}
+            >
+              <Grid
+                container
+                display="flex"
+                direction="column"
+                paddingTop={0}
+                spacing={0}
+                justifyContent="center"
+                wrap="nowrap"
+                alignItems="center"
+                sx={{ background: "gray", height: "60vh", width: "50vh" }}
+              >
+                <Grid item marginTop={5} marginBottom={5}>
+                  <TextField
+                    className="form-item"
+                    required
+                    id="outlined-basic"
+                    label="Title"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item marginBottom={5}>
+                  <TextField
+                    className="form-item"
+                    id="outlined-basic"
+                    label="Type"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item marginBottom={5}>
+                  <TextField
+                    className="form-item"
+                    id="outlined-basic"
+                    label="Catagory"
+                    variant="outlined"
+                  ></TextField>
+                </Grid>
+                <Grid item marginBottom={5}>
+                  <TextField
+                    className="form-item"
+                    id="outlined-basic"
+                    label="Store"
+                    variant="outlined"
+                    placeholder="Select"
+                    value={this.store}
+                    onChange={this.onChangeStore}
+                    select
+                    helperText={`Please select "TBD" if unknown!`}
+                  >
+                    {stores.map((option) => (
+                      <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                      </MenuItem>
+                    ))}
+                  </TextField>
+                  <Grid direction="row" display="flex" marginRight={3}>
+                    <Grid item marginTop={1}>
+                      <Button variant="contained" color="success">
+                        Submit
+                      </Button>
                     </Grid>
-                )
-                }
-            </form>
-        )
-    }
+                    <Grid item marginTop={1}>
+                      <Button variant="contained" color="success">
+                        Submit
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          )}
+        </form>
+      </ThemeProvider>
+    );
+  }
 }
