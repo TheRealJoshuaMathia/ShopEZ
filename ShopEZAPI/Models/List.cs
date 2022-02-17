572200f340c72ed85d6f19de130d4a88047315b3
joshuamathia <joshuamathia@pnch.xyz>
2022-02-16 18:31:55 -0800

    adding home & list classes

---
+++ ShopEZAPI/Models/List.cs
@@ -0,0 +1,22 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ShopEZAPI.Models;
+private class ShoppingList {
+
+    private List<Item>shoppingList;
+    public List()
+    {
+        this.shoppingList = new List<Item>();
+    }
+    private void editQuanitiy(Item ItemId) {
+
+        while (shoppingList != null ) {
+            
+        }
+
+    }
+
+}
+
+
